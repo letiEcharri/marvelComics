5e4d8e722ab6d3fada0d6b98c4af06cd323fb5c9
Leticia <leticia.echarri@gmail.com>
2024-03-09 18:11:15 +0100

    Create project

---
+++ marvelComics/marvelComics/marvelComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvelComicsApp.swift
+//  marvelComics
+//
+//  Created by Leticia Echarri on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct marvelComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
